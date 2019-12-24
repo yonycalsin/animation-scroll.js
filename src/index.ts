46f6ede4456d14f12fe3b420f5b0ace865616362
yonicb <yonicb20@gmail.com>
2019-12-24 17:42:57 -0500

    feat: 🎸 Created index.ts main file

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+/*!
+ * Bootstrap v1.0.0 (https://github.com/yonicb/animation-scroll.js)
+ * Copyright 2019 The Animation Scroll Js Authors
+ * Copyright 2019 Yoni Calsin <@yonicb>.
+ * Licensed under MIT (https://github.com/yonicb/animation-scroll.js/blob/master/LICENSE)
+ */
