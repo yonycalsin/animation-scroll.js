431ec63bfe2e7248091d14bdbdc03417008dcade
yonicb <yonicb20@gmail.com>
2020-01-12 11:38:15 -0500

    ci: 🎡 Callback Param is Optional

--- index.d.ts
+++ index.d.ts
@@ -5,7 +5,7 @@ interface Window {
 }
 declare class AnimationScrollJs {
    private _rAF;
-   constructor(el: any, duration: number, callback: Function, context?: any);
+   constructor(el: any, duration: number, callback?: Function, context?: any);
    private getTop;
    private easeInOut;
    private getPosition;
